7f86531cd37b3fc83f0a370130d260e876982fe3
1 <1@DESKTOP-39PRQAB>
2025-04-08 13:40:48 +0300

    Добавьте файлы проекта.

---
+++ ExamApp/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private int _score1, _score2, _score3, _score4;
+    private string _resultText;
+
+    public int Score1
+    {
+        get => _score1;
+        set { _score1 = Validate(value); OnPropertyChanged(); }
+    }
+
+    public int Score2
+    {
+        get => _score2;
+        set { _score2 = Validate(value); OnPropertyChanged(); }
+    }
+
+    public int Score3
+    {
+        get => _score3;
+        set { _score3 = Validate(value); OnPropertyChanged(); }
+    }
+
+    public int Score4
+    {
+        get => _score4;
+        set { _score4 = Validate(value); OnPropertyChanged(); }
+    }
+
+    public string ResultText
+    {
+        get => _resultText;
+        set { _resultText = value; OnPropertyChanged(); }
+    }
+
+    public ICommand CalculateCommand { get; }
+
+    public MainViewModel()
+    {
+        CalculateCommand = new RelayCommand(Calculate);
+    }
+
+    public void Calculate()
+    {
+        int sum = Score1 + Score2 + Score3 + Score4;
+        string grade;
+        if (sum >= 90) grade = "5";
+        else if (sum >= 75) grade = "4";
+        else if (sum >= 60) grade = "3";
+        else grade = "2";
+        ResultText = $"Сумма: {sum}\nОценка: {grade}";
+    }
+
+    private int Validate(int value)
+    {
+        if (value < 0) return 0;
+        if (value > 25) return 25;
+        return value;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ ExamTest/ExamTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExamApp; 
+
+[TestClass]
+public class ExamTests
+{
+    [TestMethod]
+    public void CalculateGrade_MaxScores_Returns5()
+    {
+        var vm = new MainViewModel
+        {
+            Score1 = 25,
+            Score2 = 25,
+            Score3 = 25,
+            Score4 = 25
+        };
+
+        vm.Calculate();
+
+        Assert.AreEqual("Сумма: 100\nОценка: 5", vm.ResultText);
+    }
+
+    [TestMethod]
+    public void Validate_ScoreAbove25_ClampsTo25()
+    {
+        var vm = new MainViewModel();
+
+        vm.Score1 = 30;
+
+        Assert.AreEqual(25, vm.Score1);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void Calculate_InvalidScores_ThrowsException()
+    {
+        var vm = new MainViewModel();
+        typeof(MainViewModel).GetProperty("Score1").SetValue(vm, -5, null);
+
+        vm.Calculate();
+    }
+}
